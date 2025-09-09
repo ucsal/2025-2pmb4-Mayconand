34e173dee11c91fa4fd4fc9b7fa84805a961a5bb
Mayconand <maycon.andrade.dev@gmail.com>
2025-09-09 07:20:26 -0300

    Clicar e arrastar: Defeni a pre-visualização. Aplicado!

--- src/main/java/br/com/mariojp/figureeditor/DrawingPanel.java
+++ src/main/java/br/com/mariojp/figureeditor/DrawingPanel.java
@@ -16,14 +16,52 @@ class DrawingPanel extends JPanel {
     private static final int DEFAULT_SIZE = 60;
     private final List<Shape> shapes = new ArrayList<>();
     private Point startDrag = null;
-
+    private Shape previewShape = null;
     DrawingPanel() {
         
-        setBackground(Color.WHITE);
+        setBackground(Color.BLACK);
         setOpaque(true);
         setDoubleBuffered(true);
 
         var mouse = new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                startDrag = e.getPoint();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                Shape s = new Ellipse2D.Double(
+                        Math.min(startDrag.x, e.getX()),
+                        Math.min(startDrag.y, e.getY()),
+                        Math.abs(startDrag.x - e.getX()),
+                        Math.abs(startDrag.y - e.getY())
+                );
+                startDrag = null;
+
+                shapes.add(s);
+                repaint();
+
+
+
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+                previewShape = new Ellipse2D.Double(
+                    Math.min(startDrag.x, e.getX()),
+                    Math.min(startDrag.y, e.getY()),
+                    Math.abs(startDrag.x - e.getX()),
+                    Math.abs(startDrag.y - e.getY())
+                    );
+
+                repaint();
+
+            }
+
+
             @Override public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 1 && startDrag == null) {
                     int size = Math.max(Math.min(DEFAULT_SIZE, DEFAULT_SIZE), 10);
@@ -32,6 +70,7 @@ class DrawingPanel extends JPanel {
                     shapes.add(s);
                     repaint();
                 }
+
             }
         };
         addMouseListener(mouse);        
@@ -55,6 +94,14 @@ class DrawingPanel extends JPanel {
             g2.setColor(new Color(0,0,0,70));
             g2.setStroke(new BasicStroke(1.2f));
             g2.draw(s);
+        }
+
+        if(previewShape != null) {
+            float[] dash = {10f, 5f};
+            Stroke dashed = new BasicStroke(2f,BasicStroke.CAP_BUTT,BasicStroke.JOIN_BEVEL,10f,dash,0f);
+            g2.setColor(new Color(30,144,255));
+            g2.setStroke(dashed);
+            g2.draw(previewShape);
         }
 
         g2.dispose();
